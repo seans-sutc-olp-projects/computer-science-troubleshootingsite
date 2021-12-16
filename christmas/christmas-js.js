a5451e749a5612008e7e524091df5f700269f70a
devramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-12-16 10:32:08 +0000

    atemptng to fix my soul

--- christmas/christmas-js.js
+++ christmas/christmas-js.js
@@ -3,12 +3,11 @@ function ischristmas() {
     const month = d.getMonth()
     return month == 11
 }
-function christmasstylesheet() {
-    if (ischristmas()) {
-        var head = document.getElementsByTagName('HEAD')[0]; 
-        var link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.href = '/christmas/christmas-css.css'; 
-        head.appendChild(link); 
-    }
+if (ischristmas()) {
+  var head = document.getElementsByTagName('HEAD')[0]; 
+  var link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = '/christmas/christmas-css.css'; 
+  head.appendChild(link); 
+  console.log('ammended the file');
 }
